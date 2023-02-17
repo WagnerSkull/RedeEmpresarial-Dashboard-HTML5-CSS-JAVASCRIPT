a2b7c7af2be1b937f926be8035089766230aa799
WagnerSkull <wagnerskull@hotmail.com>
2023-02-17 15:46:55 -0300

    finishedResponsivenessAndStartingJavaScript

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-//Loanding...
+//Menu lateral
+const sideMenu = document.querySelector("aside");
+
+//Botão Menu
+const menuBtn = document.querySelector("#menu-btn");
+
+//Botão Close fechar X 
+const closeBtn = document.querySelector("#close-btn")
+
+//Pra aparecer o menu
+menuBtn.addEventListener("click", () => {
+    sideMenu.style.display = "block";
+})
+
+//Pra esconder o menu
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display ='none';
+})
+
+//
