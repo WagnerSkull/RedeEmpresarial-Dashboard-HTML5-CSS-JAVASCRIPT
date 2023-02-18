586d374afc5962f0ec6293bd446557abd89ff541
WagnerSkull <wagnerskull@hotmail.com>
2023-02-18 06:50:09 -0300

    +CSS+JS

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 //Menu lateral
 const sideMenu = document.querySelector("aside");
 
+
 //Botão Menu
 const menuBtn = document.querySelector("#menu-btn");
 
+
 //Botão Close fechar X 
 const closeBtn = document.querySelector("#close-btn")
+
+
+//Botão Tema Dark
+const themeToggler = document.querySelector(".theme-toggler");
+
 
 //Pra aparecer o menu
 menuBtn.addEventListener("click", () => {
     sideMenu.style.display = "block";
 })
 
+
 //Pra esconder o menu
 closeBtn.addEventListener('click', () => {
-    sideMenu.style.display ='none';
+    sideMenu.style.display = 'none';
 })
 
-//
+//Alterar o Tema Black&White
+themeToggler.addEventListener('click', () =>{
+    document.body.classList.toggle('dark-theme-variables');
+
+    //Pra alterar o background do span do tema pra colocar no selecionado.
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
