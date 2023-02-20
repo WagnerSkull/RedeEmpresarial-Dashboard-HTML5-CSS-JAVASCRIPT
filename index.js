e743e1788e1e6def7a3f2495aa1484cb7b0a6b2d
WagnerSkull <wagnerskull@hotmail.com>
2023-02-20 09:02:56 -0300

    organOrdersJS

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const menuBtn = document.querySelector("#menu-btn");
 
 
 //Botão Close fechar X 
-const closeBtn = document.querySelector("#close-btn")
+const closeBtn = document.querySelector("#close-btn");
 
 
 //Botão Tema Dark
@@ -26,10 +26,30 @@ closeBtn.addEventListener('click', () => {
 })
 
 //Alterar o Tema Black&White
-themeToggler.addEventListener('click', () =>{
+themeToggler.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme-variables');
 
     //Pra alterar o background do span do tema pra colocar no selecionado.
     themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
-})
+});
+
+//Preenchendo a tabela de pedidos
+Orders.forEach(order => {
+
+    //Criar a linha TR
+    const tr = document.createElement('tr');
+
+    //Conteúdo da linha
+    const trContent = `
+                           <td>${order.productName}</td>
+                            <td>${order.productNumber}</td>
+                            <td>${order.paymentStatus}</td>
+                            <td class="warning">${order.shipping === 'pending' ? 'warning' : 'primary'}">${order.shipping}</td>
+                            <td class="primary">Details</td>`;
+                            
+                            //
+                            tr.innerHTML = trContent;
+                            document.querySelector
+                            
+});
---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lavadora KF202',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Light Bar F11',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Sniper PRO Redragon',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Acer Nitro5',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Elise PRO Redragon',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Mouse Pad T-Dagger Lava',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
